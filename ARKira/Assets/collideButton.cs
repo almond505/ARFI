c7f37f8ddcd907d01e57a7b8494563d9f86d848f
Almond Kira <almond505@gmail.com>
2020-04-25 17:48:28 +0700

    My files

---
+++ ARKira/Assets/collideButton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collideButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("BALLL");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Cube") {
+            collision.gameObject.GetComponent<Renderer>().material.color = Color.blue;
+            Debug.Log("Crash ONE");
+        }
+        if (collision.gameObject.name == "Cube2")
+        {
+            collision.gameObject.GetComponent<Renderer>().material.color = Color.blue;
+            Debug.Log("Crash TWO");
+        }
+        if (collision.gameObject.name == "Cube3")
+        {
+            collision.gameObject.GetComponent<Renderer>().material.color = Color.blue;
+            Debug.Log("Crash THREE");
+        }
+    }
+}
